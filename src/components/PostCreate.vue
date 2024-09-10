017aaf8f2f259a892ccabded856958fb51dd6110
phygloria <phygloria@gmail.com>
2024-09-10 18:12:20 +0900

    0910

--- src/components/PostCreate.vue
+++ src/components/PostCreate.vue
@@ -2,45 +2,62 @@
 
   <MainTop />
 
-  <div>
-    <h2>Q & A</h2>
-    <form @submit.prevent="createPost">
-      <div>
-        <label for="title">제목:</label>
-        <input id="title" v-model="post.title" required>
-      </div>
-      <div>
-        <label for="author">글쓴이:</label>
-        <input id="author" v-model="post.author" required>
-      </div>
-      <div>
-        <label for="content">내용:</label>
-        <textarea id="content" v-model="post.content" required></textarea>
-      </div>
-      
-      <br>
-      <!-- 이미지 업로드 -->
-      <div v-if="!previewUrl">
-        <input type="file" id="upload-image" @change="onFileSelected" hidden accept="image/png, image/jpeg, image/jpg"/>
-        <label for="upload-image">
-          이미지 선택 :   <br>
-          <img class="imageUpIcon" @change="onFileSelected" src="@/assets/images/create/icon_upload.png" alt="Upload icon"/>
-        </label>
-      </div>
-      <!-- 선택한 이미지 미리보기 -->
-      <div v-if="previewUrl" class="preview=container">
-        <img :src="previewUrl" alt="Preview" style="max-width: 300px;">
-        <br>
-        <button @click="removePreview" class="remove-preview">X</button>
-      </div>
-      
-      <div v-if="uploadStatus">{{ uploadStatus }}</div>
+<div class="body">
+  <div class="bodyLine">
+    <div>
+      <h2 class="question">질문하기</h2>
+      <div class="bodyContainer">
+        <div class="bodyBar">
+        <form @submit.prevent="createPost">
+          <div class="input-row">
+            <div class="input-wrapper title-wrapper">
+              <input id="title" v-model="post.title" required placeholder="제목">
+              <span class="input-label">Title</span>
+            </div>
+            <div class="input-wrapper author-wrapper">
+              <input id="author" v-model="post.author" required placeholder="글쓴이">
+              <span class="input-label">User</span>
+            </div>
+          </div>
+          <div class="content-wrapper">
+            <div class="input-wrapper">
+              <textarea id="content" v-model="post.content" required placeholder="질문내용"></textarea>
+              <span class="input-label">Question</span>
+            </div>
+            
+            <br>
+            <!-- 이미지 업로드 -->
+            <div class="image-upload-area">
+              <div v-if="!previewUrl">
+                <span class="image-upload-text">이미지 선택 </span>
+                <input type="file" id="upload-image" @change="onFileSelected" hidden accept="image/png, image/jpeg, image/jpg"/>
+                <label for="upload-image">
+                  <img class="imageUpIcon" src="@/assets/images/create/icon_upload.png" alt="Upload icon"/>
+                </label>
+              </div>
+            </div>
+          </div>
+          <!-- 선택한 이미지 미리보기 -->
+          <div v-if="previewUrl" class="preview=container">
+            <img :src="previewUrl" alt="Preview" style="max-width: 300px;">
+            <br>
+            <button @click="removePreview" class="remove-preview">X</button>
+          </div>
+          
+          <div v-if="uploadStatus">{{ uploadStatus }}</div>
+          
 
-      <br>
-      <!-- 작성완료 버튼 -->
-      <button type="submit">게시글 작성</button>
-    </form>
+          <br>
+          <!-- 작성완료 버튼 -->
+          <div class="btn">
+            <button type="submit" id="submit">질문하기</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script setup>
@@ -126,12 +143,171 @@ const uploadImage = async () => {
 </script>
 
 <style scoped>
+
+.body {
+  padding-right: 30px;
+  padding-left: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.bodyLine {
+  width: 100%;
+  height: 100%; 
+  background: white; 
+  box-shadow: 0px 3px 7px #DBFA5F; 
+  border-radius: 50px; 
+  border: 3px #8CD000 solid;
+  padding: 20px;
+  margin-top: 20px;
+}
+
+.bodyBar {
+  margin: 0 20% 5% 20%;
+}
+
+.input-row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  width: 100%;
+}
+
+.input-wrapper {
+  position: relative;
+}
+
+.title-wrapper {
+  flex: 3;
+  margin-right: 15px;
+}
+.author-wrapper {
+  flex: 1;
+}
+
+.input-wrapper + .input-wrapper {
+  margin-left: 20px;
+}
+
+.input-label {
+  position: absolute;
+  left: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #555555;
+  font-size: 14px;
+  pointer-events: none;
+}
+
+textarea + .input-label {
+  top: 20px;
+  transform: none;
+}
+
+input, textarea {
+  width: 100%;
+  padding: 10px 15px 10px 70px;
+  border: 2px #555555 solid;
+  border-radius: 20px; 
+  box-shadow: 0px 3px 7px #555555; 
+  font-size: 15px;
+  background: white;
+}
+
+input::placeholder, textarea::placeholder {
+  opacity: 0.5;
+}
+
+.question {
+  color: #707070;
+  font-size: 70px;
+  font-family: MangoByeolbyeol;
+  font-weight: 400;
+  word-wrap: break-word;
+  margin-bottom: 20px;
+}
+
+#title {
+  width: 100%; height: 50px; background: white; box-shadow: 0px 3px 7px #c4c4c4; border-radius: 20px; border: 3px #ffffff solid;
+}
+#author {
+  width: 100%; height: 50px; background: white; box-shadow: 0px 3px 7px #c4c4c4; border-radius: 20px; border: 3px #ffffff solid;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  
+}
+label {
+  margin-bottom: 5px;
+  color: #666;
+}
+input[type="text"], textarea {
+  width: 100%;
+  height: 100%; 
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 2px #8CD000 solid;
+  border-radius: 20px; 
+  box-shadow: 0px 3px 7px #B3E000; 
+  font-size: 15px;
+  background: white; 
+}
+
+.content-wrapper {
+  position: relative;
+  width: 100%;
+}
+
+textarea {
+  width: 100%;
+  height: 350px;
+  resize: vertical;
+  padding-top: 50px;
+  padding-left: 30px;
+  padding-right: 120px; /* 오른쪽에 아이콘을 위한 공간 확보 */
+  box-shadow: 0px 3px 7px #c4c4c4; border-radius: 20px; border: 3px #ffffff solid;
+}
+.image-upload-area {
+  position: absolute;
+  top: 40px;
+  right: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.image-upload-text {
+  color: #555555;
+  font-size: 15px;
+  margin-bottom: 5px;
+  margin-right: 10px;
+}
+
 .imageUpIcon {
+  position: absolute;
+  top: 30px;
+  right: 0px;
   width: 100px; 
   height: auto; 
   opacity: 0.3; 
-  border: 1px solid black;
+  border: 1px solid #555555;
+  cursor: pointer;
+  z-index: 1; /* textarea 위에 표시되도록 */
 }
+.upload-image {
+  position: absolute;
+  top: 520px;
+  right: 70px;
+  width: 100px; 
+  height: auto; 
+  opacity: 0.3; 
+  border: 1px solid #555555;
+  cursor: pointer;
+  z-index: 1; /* textarea 위에 표시되도록 */
+}
+
 .preview-container {
   position: relative;
   display: inline-block;
@@ -148,10 +324,36 @@ const uploadImage = async () => {
   cursor: pointer;
 }
 
-.imageUpIcon {
-  width: 100px; 
-  height: auto; 
-  opacity: 0.3; 
-  border: 1px solid black;
+.btn{
+  width: 100%; height: 100%; position: relative;
 }
+#submit {
+  width: 200px; height: 50px; background: #707070; box-shadow: 0px 3px 7px #c4c4c4; border-radius: 20px; border: 3px #ffffff solid;
+  color: white;
+  font-size: 20px;
+  font-family: Mango Ddobak;
+  font-weight: 400;
+  letter-spacing: 2.50px;
+  word-wrap: break-word;  
+  cursor: pointer; /* 마우스 커서를 포인터로 변경 */
+  transition: background-color 0.2s ease; /* 부드러운 색상 전환 효과 */
+}
+#submit:hover {
+  background: #8CD000;
+}
+
+
+@media (max-width: 768px) {
+  .input-row {
+    flex-direction: column;
+  }
+
+  .title-wrapper, .author-wrapper {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 15px;
+  }
+}
+
+
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+       <MainTop />
+       <br>
+       <HomeBody />
+    </div>
+</template>
+  
+<script>
+import MainTop from '@/components/MainTop.vue';
+import HomeBody from '@/components/HomeBody.vue';
+
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      MainTop,
+      HomeBody
+    }
+  }
+</script>
